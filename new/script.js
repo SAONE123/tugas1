e33e75ff4e62747f4e00377ce14861d08f3b6460
Your Name <you@example.com>
2023-08-26 14:41:35 +0700

    new code for photos resume

--- new/script.js
+++ new/script.js
@@ -3,3 +3,15 @@ $(document).ready(function () {
   jQuery(".materialbox").materialbox();
   jQuery(".parallax").parallax();
 });
+
+var pop = document.getElementById("photos");
+var popin = false;
+function viewpic() {
+  if (popin) {
+    pop.style.display = "none";
+  } else {
+    pop.style.display = "block";
+    alert("please click again for close photos windows");
+  }
+  popin = !popin;
+}
--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+var nama = document.getElementById("nama");
+var role = document.getElementById("role");
 var avability = document.getElementById("avability");
 var age = document.getElementById("age");
 var lokasi = document.getElementById("lokasi");
 var years = document.getElementById("years");
 var email = document.getElementById("email");
 function input() {
+  document.getElementById("input_nama").innerText = nama.value;
+  document.getElementById("input_role").innerText = role.value;
   document.getElementById("input_avability").innerText = avability.value;
   document.getElementById("input_age").innerText = age.value + " " + "tahun";
   document.getElementById("input_lokasi").innerText = lokasi.value;
   document.getElementById("input_years").innerText = years.value;
   document.getElementById("input_email").innerText = email.value;
+}
+
+if(nama).style.value = true{
+  Text.style.size = ("20px");
+}else{
+  Text.style.size = ("30px");
 }
