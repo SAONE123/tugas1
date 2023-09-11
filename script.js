54eb1d74d76cfbe0a9f1cd22b0a587a4e9eab8ce
Alfian <titanalfian112@gmail.com>
2023-09-11 22:18:02 +0700

    new

--- script.js
+++ script.js
@@ -9,8 +9,11 @@ function input() {
   document.getElementById("input_nama").innerText = nama.value;
   document.getElementById("input_role").innerText = role.value;
   document.getElementById("input_avability").innerText = avability.value;
-  document.getElementById("input_age").innerText = age.value + " " + "tahun";
+  document.getElementById("input_age").innerText = age.value + " " + "Tahun";
   document.getElementById("input_lokasi").innerText = lokasi.value;
-  document.getElementById("input_years").innerText = years.value;
+  document.getElementById("input_years").innerText = years.value + "" + "Tahun";
   document.getElementById("input_email").innerText = email.value;
+}
+function resume() {
+  alert("harap terhubung dengan internet sebelum membuka situs ini");
 }
